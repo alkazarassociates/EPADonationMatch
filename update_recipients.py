8c9b8fa7981ce089fd0e47171b61d7097b16ec87
Mike Elkins <Mike@Elkins.com>
2024-02-27 19:40:45 -0500

    Write out result of update_recipients.

--- update_recipients.py
+++ update_recipients.py
@@ -7,6 +7,36 @@ import sys
 import donation_data
 
 
+def report(result: donation_data.UpdateRecipientResult, data: donation_data.State) -> str:
+    if not result.success:
+        ret = "Errors detected--did not update recipients!"
+        if result.errors:
+            ret += "---Errors---"
+            for e in result.errors:
+                ret += "\n"
+                ret += e
+        if result.warnings:
+            ret += "---Warnings---"
+            for w in result.warnings:
+                ret += "\n"
+                ret += w
+        return ret
+    assert len(result.errors) == 0
+    ret = f"Added {result.new_count} recipients, for a total of {len(data.recipients)}.\n"
+    if result.warnings:
+        ret += "\n"
+        ret += "---Warnings---"
+        for w in result.warnings:
+            ret += "\n"
+            ret += w
+    return ret
+
+
+def write_validation_report(args, result: donation_data.UpdateRecipientResult, data: donation_data.State) -> None:
+    # TODO Implement
+    pass
+
+
 def Main():
     parser = argparse.ArgumentParser(
         prog='update_recipients',
@@ -17,9 +47,13 @@ def Main():
 
     data = donation_data.load_state(args)
 
-    data.update_recipients(donation_data.load_csv(args.recipients))
+    result = data.update_recipients(donation_data.load_csv(args.recipients))
 
-    donation_data.save_state(args, data)
+    if result.success:
+        donation_data.save_state(args, data)
+        write_validation_report(args, result, data)
+
+    print(report(result, data))
 
 
 if __name__ == '__main__':
